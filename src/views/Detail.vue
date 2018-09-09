94c983394c86f2dc019af3aca8b903b0facb6f54
yuan3140 <562720180@qq.com>
2018-09-09 23:58:14 +0800

    add carshow

---
+++ src/views/Detail.vue
@@ -0,0 +1,148 @@
+<template>
+   <div class="container">
+		<div class="logo" @click="$router.push('/index')">
+			<img class="logo-img" src="../assets/image/back.png" alt="">
+		</div>
+		<div class="header">
+			<span class="title">BMW 4系敞篷轿跑车</span>
+			<span class="price">官方指导价：40~50万</span>
+		</div>
+		<div class="content">
+			<div class="big">
+				<div class="tip">
+					<p>佳木斯宝马4Sd店秋季特惠活动</p>
+					<span>活动详情</span>
+				</div>
+				<img src="../assets/image/car.png" alt="">
+				<div class="color">
+					<p>颜色</p>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+			<div class="menu">
+				<div class="item last"></div>
+				<div class="item car-show">
+					<span>BMW 6系四门轿车</span>
+					<img src="../assets/image/car1.png" alt="">
+				</div>
+				<div class="item car-show">
+					<span>创新BMW 6系四GT</span>
+					<img src="../assets/image/car2.png" alt="">
+				</div>
+				<div class="item car-show">
+					<span>BMW 6系四门跑车</span>
+					<img src="../assets/image/car3.png" alt="">
+				</div>
+				<div class="item next"></div>
+			</div>
+		</div>
+		<div class="bottom">
+			
+		</div>
+	</div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+<style scoped>
+.container {
+			height: 100%;
+			position: relative;
+			background: #051625;
+		}
+		.logo {
+			position: absolute;
+			top: 20px;
+			right: 20px;
+			width: 96px;
+			height: 84px;
+			cursor: pointer;
+		}
+		.logo-img {
+			width: 96px;
+			height: 84px;
+		}
+		.header {
+			padding-top: 126px;
+			color: #fff;
+		}
+		.header .title{
+			margin-left: 200px;
+			font-size: 34px;
+		}
+		.header .price{
+			margin-left: 120px;
+			font-size: 24px;
+		}
+		.content {
+			margin-top: 76px;
+		}
+		.content .big {
+			display: flex;
+			justify-content: center;
+			color: #fff;
+		}
+		.big .tip {
+			width: 300px;
+			color: #fff;
+		}
+		.big img {
+			width: 860px;
+			height: 386px;
+		}
+		.color {
+			display: flex;
+			flex-direction: column;
+			align-items: flex-end;
+		}
+		.color p {
+			width: 300px;
+			color: #fff;
+			text-align: right;
+		}
+		.color span {
+			display: inline-block;
+			width: 12px;
+			height: 12px;
+			background: #fff;
+			border: 6px;
+			margin-bottom: 10px;
+			cursor: pointer;
+		}
+		.content .menu {
+			display: flex;
+			justify-content: center;
+			margin-top: 20px;
+		}
+		.menu .item {
+			width: 294px;
+			height: 304px;
+			cursor: pointer;
+			color: #fff;
+		}
+		.menu .car-show {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+		}
+		.car-show span {
+			margin-top: 20px;
+		}
+		.car-show img {
+			margin-bottom: 40px;
+		}
+		.menu .last {
+			background: url('../assets/image/last.png') no-repeat center center;
+		}
+		.menu .next {
+			background: url('../assets/image/next.png') no-repeat center center;
+		}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,220 @@
+<template>
+   <div class="container">
+		<div class="main">
+			<div class="box">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/bmw.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">宝景宝马4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+			<div class="box" @click="showDetail">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/奔驰.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">宝景奔驰4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+			<div class="box" @click="showDetail">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/大众.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">宝景大众4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+			<div class="box" @click="showDetail">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/别克.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">宝景别克4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+			<div class="box" @click="showDetail">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/现代.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">北京现代4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+			<div class="box" @click="showDetail">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/玛莎拉蒂.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">玛莎拉蒂4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+			<div class="box" @click="showDetail">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/凯迪拉克.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">凯迪拉克4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+			<div class="box" @click="showDetail">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/名爵.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">宝景名爵4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+			<div class="box" @click="showDetail">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/奥迪.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">宝景奥迪4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+			<div class="box" @click="showDetail">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/马自达.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">马自达4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+			<div class="box" @click="showDetail">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/长安.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">长安汽车4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+			<div class="box" @click="showDetail">
+				<div class="logo">
+					<img class="logo-img" src="../assets/image/兰博基尼.png">
+				</div>
+				<div class="contract">
+					<div>
+						<img class="wechat-img" src="../assets/image/wechat.png" alt="wechat">
+					</div>
+					<span class="name">兰博基尼4S店</span>
+					<span class="tel">电话：11234568944</span>
+				</div>
+			</div>
+		</div>
+		<div class="bottom">
+			
+		</div>
+	</div>
+</template>
+<script>
+export default {
+  data () {
+      return {
+
+      }
+  },
+  methods: {
+  	showDetail () {
+  		this.$router.push('/detail')
+  	}
+  }
+}
+</script>
+<style scoped>
+	* {
+		box-sizing: border-box;
+	}
+		.container {
+			background: #051625;
+			height: 100%;
+		}
+		.main	{
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-between;
+			padding: 0 80px;
+		}
+		.box {
+			width: 400px;
+			height: 270px;
+			display: flex;
+			background: #fff;
+			margin-top: 44px;
+			padding: 30px 0;
+			cursor: pointer;
+		}
+		.logo {
+			width: 170px;
+			height: 200px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border-right: 2px solid #000;
+		}
+		.logo-img {
+			width: 120px;
+			height: 120px;
+		}
+		.contract {
+			flex: 1;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
+		.wechat-img {
+			width: 102px;
+			height: 102px;
+		}
+		.name {
+			margin-top: 18px;
+		}
+		.tel {
+			margin-top: 22px;
+		}
+</style>
